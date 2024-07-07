750b811642cb5fa4ed9346df34fcb83c6291c9b1
Samin <saminc@kth.se>
2024-07-07 21:23:14 +0200

    Testing FrameWork

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,72 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let content;
+  let isLoggedIn = true;
+  if (isLoggedIn) {
+    content = <Admin />;
+  } else {
+    content = <User />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='Test'>
+    <h1>TFT Helper</h1>
+    <MyButton/>
+    <AboutPage className ='Test2'></AboutPage>
+    {content}
+   </div>
+  
+  );
+}
+
+function MyButton(){
+  return (
+    <button className='ButtonFire'> Issa button</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div className='Test2'>
+      <h1>About</h1>
+      <p>Hello {userTest.name + ' ' + userTest.lastName} this issa about page</p>
+      <img className='avatar' src={userTest.imageUrl}></img>
     </div>
   );
 }
+
+const userTest = {
+  name: 'User',
+  lastName: 'Chowdhury',
+  imageUrl: 'https://i.imgur.com/H3vo6s7.png',
+
+};
+
+const AdminTest = {
+  name: 'Admin',
+  lastName: 'Chowdhury',
+  imageUrl: 'https://i.imgur.com/qYr3vp8.png',
+
+};
+
+function Admin(){
+  return (
+    <div>
+      <img src = {AdminTest.imageUrl}></img>
+    </div>
+  );
+}
+
+
+function User(){
+  return (
+    <div>
+      <img src = {userTest.imageUrl}></img>
+    </div>
+  );
+}
+
+
 
 export default App;
